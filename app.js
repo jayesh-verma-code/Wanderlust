6c35e14b2c57e90bf6ae3a3816bfc4a879393096
jayesh-verma-code <jayeshvermaformalwork@gmail.com>
2026-05-30 15:48:33 +0530

    Phase 1 - Explore EJS-Mate for layouts

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-Mate');
 
 const Listing = require('./models/listing.js');
 
@@ -11,6 +12,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
